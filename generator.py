3f7eb2124c6476ab62631f4b31defc0c974814d1
Tony <araktan.dev@gmail.com>
2025-01-20 16:18:04 +0100

    split generator into separate file

---
+++ generator.py
@@ -0,0 +1,45 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+import pandas as pd
+import numpy as np
+
+def get_x_y(smiles_string, image_dims=(60, 60)):
+    # TODO: needs assertion that the string is good   
+    mol = Chem.MolFromSmiles(smiles_string)
+    img = Draw.MolToImage(mol, size=image_dims)
+    thresh = 200
+    fn = lambda x : 0 if x > thresh else 255
+    r = img.convert('L').point(fn, mode='1')
+    #r = img.convert('L')
+    adj = Chem.GetAdjacencyMatrix(mol)
+    padded_adj = np.zeros(image_dims)
+    for index, bond_val in np.ndenumerate(adj):
+        padded_adj[index] += bond_val
+    return np.expand_dims(np.array(r), -1), np.expand_dims(padded_adj, -1)
+
+def main():
+    # generte data
+    df = pd.read_csv('data/SMILES.csv')
+    output_shape = (60, 60)
+    x_data, y_data = [],[]
+    for SMILES in df['SMILES']:
+        try:
+            img, adj_matrix = get_x_y(SMILES, image_dims=output_shape)
+            x_data.append(img)
+            y_data.append(adj_matrix)
+        except:
+            continue
+
+    x_data, y_data = np.array(x_data), np.array(y_data)
+    
+    with open('test_np_X.npy', 'wb') as f:
+        np.save(f, x_data, allow_pickle=True)
+    
+    with open('test_np_Y.npy', 'wb') as f:
+        np.save(f, y_data, allow_pickle=True)
+    
+    return
+
+if __name__ == '__main__':
+    main()
+
